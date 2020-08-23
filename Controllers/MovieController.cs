3be3f1cf1a6ea6e2dc374d405f4d0b5e5a547c09
ramez <herrickramez@gmail.com>
2020-08-23 14:18:50 -0400

    submission

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab21_MovieRegisteration.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab21_MovieRegisteration.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new Movie());
+        }
+        public IActionResult MovieResult(Movie movie)
+        {
+            if(ModelState.IsValid)
+            {
+                return View(movie);
+            }
+
+            return View("Index", movie);
+        }
+
+        public IActionResult WatchTrailer ()
+        {
+            return View();
+        }
+        public IActionResult PlayTrailer(Movie movie)
+        {
+            return View(movie);
+        }
+
+
+    }
+}
